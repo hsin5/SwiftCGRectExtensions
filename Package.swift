5125c262e3717015f251192715b04c24217f72bd
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 18:04:41 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -21,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CGRectExtensions",
+            path: "CGRectExtensions",
             dependencies: []),
         .testTarget(
             name: "CGRectExtensionsTests",
+            path: "CGRectExtensionsTests",
             dependencies: ["CGRectExtensions"]),
     ]
 )
