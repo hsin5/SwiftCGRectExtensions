3bb309240cb9daecf66149c35851821fb6aa6734
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 20:00:21 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -6,6 +6,10 @@ import PackageDescription
 
 let package = Package(
     name: "CGRectExtentions",
+    platforms: [.macOS(.v10_10),
+                .iOS(.v9),
+                .tvOS(.v9),
+                .watchOS(.v2)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
